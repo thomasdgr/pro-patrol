763726cc11a39da5bd75af25c1f54ca4c5900908
UnknownPNJ <152412264+UnknownPNJ@users.noreply.github.com>
2024-01-06 13:49:52 +0100

    changement du script

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 var audio_player : AudioStreamPlayer
 
@@ -6,10 +6,10 @@ var niveau = ["1", "2", "3", "4", "5"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Control/Label/btn_music").toggled.connect(_on_checked)
-	get_node("Control/Label/btn_exit").toggled.connect(_on_checked_exit)
-	get_node("Control/Label/btn_start").toggled.connect(_on_checked_start)
-	get_node("Control/Label/btn_niveau").toggled.connect(_on_checked_level)
+	get_node("Label/btn_music").toggled.connect(_on_checked)
+	get_node("Label/btn_exit").toggled.connect(_on_checked_exit)
+	get_node("Label/btn_start").toggled.connect(_on_checked_start)
+	get_node("Label/btn_niveau").toggled.connect(_on_checked_level)
 
 	audio_player = $AudioStreamPlayer  # Assurez-vous de remplacer 'AudioStreamPlayer' par le nom correct de votre nœud AudioStreamPlayer
 
@@ -36,7 +36,7 @@ func _on_checked_start(btn_pressed):
 		print(main_lvl.level)
 		# je veux la variable ici du script level_pop
 		# ----------- Change ici la scène pour le clic sur start ------------
-		get_tree().change_scene_to_file("res://scenes/map.tscn")
+		#get_tree().change_scene_to_file("res://menu.tscn")
 		
 		
 func _on_checked_level(btn_pressed):
