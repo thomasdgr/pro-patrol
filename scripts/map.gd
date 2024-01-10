f1f764e31d7bba139875ce37ab185351a6265d53
dorianbernasconi <dorian.bernasconi7@gmail.com>
2024-01-10 20:26:24 +0100

    tkt

--- scripts/map.gd
+++ scripts/map.gd
@@ -4,8 +4,6 @@ extends StaticBody3D
 @onready var animation_cars : Array = []
 @onready var city_sound : AudioStreamPlayer = $city_ambiance
 
-var texture_pnj_list : Array = ["PolygonCity_Texture_01_B.png","PolygonCity_Texture_01_C.png","PolygonCity_Texture_02_A.png","PolygonCity_Texture_02_B.png","PolygonCity_Texture_02_C.png","PolygonCity_Texture_03_A.png","PolygonCity_Texture_03_B.png","PolygonCity_Texture_03_C.png","PolygonCity_Texture_04_A.png","PolygonCity_Texture_04_B.png"]
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("/root/Coordinator").start_scenario(MainLvl.level) 
@@ -18,16 +16,14 @@ func _ready():
 
 func display_cars():
 	var pnj_scene = null
-	for i in range(2): # Change this range to the number of PNJ you want
-		if(i %2):
-			pnj_scene = preload("res://scenes/vehicule/Bus.tscn")
-		else:
-			pnj_scene = preload("res://scenes/vehicule/Car.tscn")
-			
-		
+	
+
+	for i in range(5): # Change this range to the number of PNJ you want
+		var random_index_vehicule = randi_range(1,3) # Generate a random offset between 0 and 1
+		pnj_scene = load("res://scenes/vehicule/vehicule"+ str(random_index_vehicule) +".tscn")
 		var pnj_instance = pnj_scene.instantiate()
 		print("MovingVehicule/Path3D" + str(i + 1) + "/PathFollow3D")
-		var animation_car = get_node("MovingVehicule/Path3D" + str(i + 1) + "/PathFollow3D")
+		var animation_car = get_node("MovingVehicule/Path3D" + str(random_index_vehicule) + "/PathFollow3D")
 		
 		var random_offset = randf() # Generate a random offset between 0 and 1
 
@@ -39,19 +35,18 @@ func display_cars():
 		animation_cars.append(animation_car)
 	
 func display_pnj():
-	for i in range(19): # Change this range to the number of PNJ you want
-		var pnj_scene = preload("res://scenes/npcs/demo_shirt.tscn")
-		var pnj_instance = pnj_scene.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED)
-		var animation_player = get_node("MovingPnj/Path3D" + str(i + 1) + "/PathFollow3D")
+	var pnj_scene
+	for i in range(24): # Change this range to the number of PNJ you want
 		
+		var random_index_pnj = randi_range(1,9) # Generate a random offset between 0 and 1
+	
+		pnj_scene = load("res://scenes/npcs/pnj/pnj" + str(random_index_pnj) +".tscn")
+		
+		var pnj_instance = pnj_scene.instantiate()
+		var animation_player = get_node("MovingPnj/Path3D" + str(i + 1) + "/PathFollow3D")
 		var random_offset = randf() # Generate a random offset between 0 and 1
-		var random_texture_index = randi() % texture_pnj_list.size() # Get a random index from your texture_pnj_list
-		var texture_path : String = "res://SourceFiles/Textures/" + texture_pnj_list[random_texture_index]
 
 		animation_player.add_child(pnj_instance)
-		
-		# Set the texture for the PNJ
-		animation_player.get_child(0).set_texture(texture_path)
 		
 		animation_player.get_child(0).walk()
 		animation_player.progress = random_offset * 100
